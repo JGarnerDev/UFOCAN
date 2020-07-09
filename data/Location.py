3eae7d3b9f5b3155f1fb6ca473cda96c91a36f03
kellan.selig <kellan.selig@gmail.com>
2020-07-08 23:39:15 -0400

    class methods expanded

--- data/Location.py
+++ data/Location.py
@@ -7,6 +7,9 @@ from ratelimiter import RateLimiter
 
 
 class Location:
+    def __init__(self):
+        self.reverse_geocode_response = []
+
     def __str__(self):
         return "Reverse Geocoding using locationiq.com"
 
@@ -23,60 +26,63 @@ class Location:
             "https://us1.locationiq.com/v1/reverse.php", params=params
         )
         content = response.json()
+        self.reverse_geocode_response.append(content)
 
-        return content
+    def return_raw(self):
+        return self.reverse_geocode_response[0]
 
-    def address(self, raw_location_info):
-        address = raw_location_info.get("display_name")
+    def address(self):
+        address = self.reverse_geocode_response[0].get("display_name")
         return address
 
-    def neighbourhood(self, raw_location_info):
-        neighbourhood = raw_location_info["address"].get("neighbourhood")
+    def neighbourhood(self):
+        neighbourhood = self.reverse_geocode_response[0]["address"].get("neighbourhood")
         return neighbourhood
 
-    def city(self, raw_location_info):
-        city = raw_location_info["address"].get("city")
+    def city(self):
+        city = self.reverse_geocode_response[0]["address"].get("city")
         return city
 
     def province(self, raw_location_info):
-        province = raw_location_info["address"].get("state")
+        province = self.reverse_geocode_response[0]["address"].get("state")
         return province
 
     def country_code(self, raw_location_info):
-        country_code = raw_location_info["address"].get("country_code")
+        country_code = self.reverse_geocode_response[0]["address"].get("country_code")
         return country_code
 
     def country(self, raw_location_info):
-        country = raw_location_info["address"].get("country")
+        country = self.reverse_geocode_response[0]["address"].get("country")
         return country
 
-    def get_all(self, raw_location_info):
-        try:
-            all_info = pd.Series(
-                [
-                    raw_location_info.get("display_name"),  # address
-                    raw_location_info["address"].get("neighbourhood"),  # neighbourhood
-                    raw_location_info["address"].get("city"),  # city
-                    raw_location_info["address"].get("state"),  # province
-                    raw_location_info["address"].get("country_code"),  # country_code
-                    raw_location_info["address"].get("country"),  # country
-                ]
-            )
-        except:
-            all_info = pd.Series(["", "", "", "", "", ""])
-        return all_info
+
+# This is for applying to a dataframe, if raw output is in a column
+def get_all(self, raw_location_info):
+    try:
+        all_info = pd.Series(
+            [
+                raw_location_info.get("display_name"),  # address
+                raw_location_info["address"].get("neighbourhood"),  # neighbourhood
+                raw_location_info["address"].get("city"),  # city
+                raw_location_info["address"].get("state"),  # province
+                raw_location_info["address"].get("country_code"),  # country_code
+                raw_location_info["address"].get("country"),  # country
+            ]
+        )
+    except:
+        all_info = pd.Series(["", "", "", "", "", ""])
+    return all_info
 
 
 def return_location_info():
-    lat = input("lat: ")
-    lon = input("lon: ")
-
     with open("/Users/kellan/Documents/GitHub/UFOCAN/data/token.txt") as f:
         private_token = f.read()
 
-    response = Location.make_request(lat, lon, private_token)
+    lat, lon = input("lat,lon: ").split(",")
+
     reverse_geocode = Location()
-    address = reverse_geocode.address(response)
+    reverse_geocode.make_request(lat, lon, private_token)
+    address = reverse_geocode.address()
 
     print(f"The Address is: {address}")
 
