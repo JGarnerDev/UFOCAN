9fb5c041114cb0dd58e0aae826cdb826ee3a83a2
Jeffe <j.garner200@gmail.com>
2020-07-19 12:25:31 -0400

    hosting, 1st attempt

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const app = express();
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	next();
@@ -57,6 +59,14 @@ app.post("/sightings", (req, res) => {
 		res.send(results);
 	});
 });
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
